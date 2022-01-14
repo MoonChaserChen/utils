f374f21f2bd27651315359516e3a55d3be72a976
chenhouzhang <chenhouzhang@meituan.com>
2022-01-14 10:37:32 +0800

    fix

--- src/test/java/test/utils/MapUtilTest.java
+++ src/test/java/test/utils/MapUtilTest.java
@@ -1,24 +1,13 @@
 package test.utils;
 
+import me.chin.utils.MapUtils;
+
 import java.util.*;
 
 /**
  * Created by ChenHouZhang on 2017/7/26.
  */
 public class MapUtilTest {
-    @org.junit.Test
-    public void testMapChange(){
-        Map<String,String[]> map = new HashMap<>();
-        map.put("a",new String[]{"1","first"});
-        map.put("b",new String[]{"2","second"});
-        Map<String, String> params2Map =MapUtils.requestParams2Map(map,"-");
-        Iterator<String> iterator = params2Map.keySet().iterator();
-        while (iterator.hasNext()) {
-            String key = iterator.next();
-            String value = params2Map.get(key);
-            System.out.println(key + " --- " + value);
-        }
-    }
 
     @org.junit.Test
     public void countWordFrequency(){
